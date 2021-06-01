e8ccef4ce428bb1e34a87a1a6fc42a6273b98f96
defini7 <75216201+defini7@users.noreply.github.com>
2021-06-01 16:01:52 +0300

    Create rgbt.cpp

---
+++ colorama/rgbt.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+#include "olcConsoleGameEngine.h"
+
+class Example : public olcConsoleGameEngine
+{
+public:
+	Example()
+	{
+		m_sAppName = L"RGBT | Colorama";
+	}
+	virtual bool OnUserCreate()
+	{
+		return true;
+	}
+	virtual bool OnUserUpdate(float fElapsedTime)
+	{
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, BG_BLACK);
+		for (int x = 0; x < ScreenWidth()/2; x++)
+		{
+			for (int y = 0; y < ScreenHeight()/2; y++)
+			{
+				Draw(x, y, PIXEL_SOLID, FG_RED);
+			}
+		}
+		for (int x = ScreenWidth() / 2; x < ScreenWidth(); x++)
+		{
+			for (int y = 0; y < ScreenHeight() / 2; y++)
+			{
+				Draw(x, y, PIXEL_SOLID, FG_GREEN);
+			}
+		}
+		for (int x = 0; x < ScreenWidth() / 2; x++)
+		{
+			for (int y = ScreenHeight()/2; y < ScreenHeight(); y++)
+			{
+				Draw(x, y, PIXEL_SOLID, FG_DARK_BLUE);
+			}
+		}
+		for (int x = ScreenWidth() / 2; x < ScreenWidth(); x++)
+		{
+			for (int y = ScreenHeight() / 2; y < ScreenHeight(); y++)
+			{
+				Draw(x, y, PIXEL_SOLID, FG_WHITE);
+			}
+		}
+		return true;
+	}
+private:
+	
+};
+
+int main()
+{
+	Example demo;
+	demo.ConstructConsole(160, 100, 8, 8);
+	demo.Start();
+	return 0;
+}
