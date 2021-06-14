d78006927570d79ca76edfa4c6b990e1a3b4577e
defini7 <75216201+defini7@users.noreply.github.com>
2021-06-14 13:43:46 +0300

    Create create_phone_number.java

---
+++ create_phone_number.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static String createPhoneNumber(int[] numbers) {
+    String result = "";
+    for (int i = 0; i < numbers.length; i++) {
+        if (i == 0) 
+            result += '(';
+        else if (i == 3) 
+            result += ") ";
+        else if (i == 6) 
+            result += '-';
+
+        result += numbers[i];
+    }
+    return result;
+  }
+}
