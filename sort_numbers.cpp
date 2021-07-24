b3bcc57792ed35135c3081754f993022f4a5c2ba
defini7 <75216201+defini7@users.noreply.github.com>
2021-07-24 20:05:10 +0500

    Create sort_numbers.cpp

---
+++ sort_numbers.cpp
@@ -0,0 +1,16 @@
+// 2 options
+#include <vector>
+
+std::vector<int> solution1(std::vector<int> nums) {
+    for (int i = 0; i < nums.size(); i++)
+      if (nums[i] > nums[i+2] && i <= nums.size() - 2)
+        std::swap(nums[i], nums[i+1]);
+    return nums;
+}
+
+#include <algorithm>
+
+std::vector<int> solution2(std::vector<int> nums) {
+  sort(nums.begin(), nums.end());
+  return nums;
+}
