644333267b4479f49b7bc44fdbd755f92dbe799a
defini7 <75216201+defini7@users.noreply.github.com>
2021-08-10 21:32:47 +0500

    Create CheckLua.cpp

---
+++ CheckLua.cpp
@@ -0,0 +1,11 @@
+// check if .lua file not contain errors
+bool CheckLua(lua_State* L, int r)
+{
+	if (r != LUA_OK)
+	{
+		std::string errormsg = lua_tostring(L, -1);
+		std::cout << errormsg << std::endl;
+		return false;
+	}
+	return true;
+}
