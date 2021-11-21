5df324f8f077bad196f038d3357c92b05234ff29
Alex <75216201+defini7@users.noreply.github.com>
2021-11-21 11:50:17 +0300

    Create flask_cookie_decoder.py

---
+++ flask_cookie_decoder.py
@@ -0,0 +1,15 @@
+import hashlib
+from itsdangerous import URLSafeTimedSerializer
+from flask.sessions import TaggedJSONSerializer
+
+salt = 'cookie-session'
+serializer = TaggedJSONSerializer()
+signer_kwargs = {
+    'key_derivation': 'hmac',
+    'digest_method': hashlib.sha1
+}
+
+# you can set secret key using "app.secret_key="yoursecretkey" in your main file"
+s = URLSafeTimedSerializer('your secret key', salt=salt, serializer=serializer, signer_kwargs=signer_kwargs)
+# cookie code after "session="
+print(s.loads('your cookie to decode'))
