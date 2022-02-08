9452ce7421fec89178d35be4f42d3542cc8d64df
Лучший <75216201+defini7@users.noreply.github.com>
2022-02-08 20:43:51 +0300

    Update sort_numbers.cpp

--- sort_numbers.cpp
+++ sort_numbers.cpp
@@ -1,16 +1,6 @@
-// 2 solutions
-#include <vector>
-
-std::vector<int> solution1(std::vector<int> nums) {
-    for (int i = 0; i < nums.size(); i++)
-      if (nums[i] > nums[i+2] && i <= nums.size() - 2)
-        std::swap(nums[i], nums[i+1]);
-    return nums;
-}
-
 #include <algorithm>
 
-std::vector<int> solution2(std::vector<int> nums) {
+std::vector<int> solution(std::vector<int> nums) {
   sort(nums.begin(), nums.end());
   return nums;
 }
