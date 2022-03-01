6cd887be2e6e9c0f86765b241a95fc7bbfa78a7c
Лучший <75216201+defini7@users.noreply.github.com>
2022-03-01 21:15:34 +0300

    Create dollar.py

---
+++ dollar.py
@@ -0,0 +1,18 @@
+import requests
+from time import sleep
+
+def do_dollar(from_currency, to_currency, api_key):
+  url= "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE" + "&from_currency=" + from_currency + "&to_currency=" + to_currency + "&apikey=" + api_key
+
+  while True:
+    req = requests.get(url)
+
+    result = req.json()
+
+    buy = result['Realtime Currency Exchange Rate']['5. Exchange Rate']
+    time = result['Realtime Currency Exchange Rate']['6. Last Refreshed']
+    print(f'{time}. Buy for: {buy}')
+
+    sleep(15)
+
+do_dollar('USD', 'RUB', '##YOUR TOKEN GOES HERE##')
